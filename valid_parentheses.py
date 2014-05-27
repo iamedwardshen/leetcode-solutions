c2dd9e0299147c7c8d1f289b7d6613eb506b08ef
rioshen <rioxshen@gmail.com>
2014-05-26 21:58:53 -0400

    add function docstring

--- valid_parentheses.py
+++ valid_parentheses.py
@@ -2,23 +2,30 @@
 
 __author__ = 'Rio'
 
-def valid(s):
-    if not s: return None
 
-    left = '([{'
-    right = ')]}'
+def valid(s):
+    """
+    Solution: If a single element belongs to left parentheses,
+    push it onto the stack, otherwise compare it with the top
+    element in the stack. Finally, the stack should be empty.
+    """
+    if s is None:  # empty string is valid.
+        return True
 
     stack = []
     for ele in s:
-        if ele in left:
+        if ele in '([{':
             stack.append(ele)
         else:
-            if len(stack):
-                opt = stack.pop()
+            if stack is not None and check_valid(ele, stack[-1]):
+                stack.pop()
             else:
                 return False
 
-            if left.index(opt) != right.index(ele):
-                return False
+    return stack is None
 
-    return True
+
+def check_valid(c1, c2):
+    return (c1 == '(' and c2 == ')'
+            or c1 == '[' and c2 == ']'
+            or c1 == '{' and c2 == '}')
