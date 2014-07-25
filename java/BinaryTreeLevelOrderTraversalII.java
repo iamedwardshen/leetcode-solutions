d45ece7d57f36f152aae2b2670a06931b0b99a91
rioshen <rioxshen@gmail.com>
2014-07-25 11:10:53 -0400

    After get the result in top-down order, reverse.

--- java/BinaryTreeLevelOrderTraversalII.java
+++ java/BinaryTreeLevelOrderTraversalII.java
@@ -10,7 +10,9 @@
 public class BinaryTreeLevelOrderTraversal {
     public List<List<Integer>> levelOrder(TreeNode root) {
         List<List<Integer>> result = new ArrayList<List<Integer>>();
-        if (root == null) return result;
+        if (root == null) {
+            return result;
+        }
 
         Queue<TreeNode> queue = new LinkedList<TreeNode>();
         queue.add(root);
@@ -21,14 +23,17 @@ public class BinaryTreeLevelOrderTraversal {
             for (int i = 0; i < size; i++) {
                 TreeNode node = queue.poll();
                 level.add(node.val);
-                if (node.left != null)  queue.add(node.left);
-                if (node.right != null) queue.add(node.right);
+                if (node.left != null) {
+                    queue.add(node.left);
+                }
+                if (node.right != null) {
+                    queue.add(node.right);
+                }
             }
 
             result.add(level);
         }
 
-        Collections.reverse(result);
         return result;
     }
 }
