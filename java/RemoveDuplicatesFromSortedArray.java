adc04eca85ec0a5f352197586e7b89b220c8a71a
rioshen <rioxshen@gmail.com>
2014-07-27 13:42:18 -0400

    Modify solution and read me

--- java/RemoveDuplicatesFromSortedArray.java
+++ java/RemoveDuplicatesFromSortedArray.java
@@ -5,17 +5,14 @@ public class RemoveDuplicatesFromSortedArray {
      *           Space Complexity - O(1), no additional space is required.
      */
     public static int removeDuplicates(int[] A) {
-        if (A == null || A.length == 0) {
-            return 0;
+        if (A == null || A.length == 0) return 0;
+
+        int occur = 2 - 1;
+        int index = occur;
+        for (int i = occur; i < A.length; i++) {
+            if (A[i] != A[index]) A[index++] = A[i];
         }
 
-        int index = 0;
-        for (int i = 0; i < A.length; i++) {
-            if (A[index] != A[i]) {
-                A[++index] = A[i];
-            }
-        }
-
-        return (index + 1);
+        return index;
     }
 }
