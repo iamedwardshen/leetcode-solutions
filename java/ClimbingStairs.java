78891b3204759321d5af955588e0fb3c4c7e0272
rioshen <rioxshen@gmail.com>
2014-07-29 07:24:06 -0400

    Add Climbing Staris solutoin

--- java/ClimbingStairs.java
+++ java/ClimbingStairs.java
@@ -1,21 +1,14 @@
 public class ClimbingStairs {
-    public int climbStairs(int n) {
-        if (n <= 0) {
-            return 0;
-        } else if (n == 1) {
-            return 1;
-        } else if (n == 2) {
-            return 2;
-        }
-        int total = 0;
-        int first = 1;
-        int second = 2;
-        for (int i = 2; i < n; i++) {
-            total = first + second;
-            first = second;
-            second = total;
-        }
+    public int clibmStaris(int n) {
+        if (n == 0) return 0;
 
-        return total;
+        int prev = 0;
+        int curr = 1;
+        for (int i = 1; i < n; i++) {
+            int tmp += curr;
+            curr += prev;
+            prev = tmp;
+        }
+        return curr;
     }
 }
